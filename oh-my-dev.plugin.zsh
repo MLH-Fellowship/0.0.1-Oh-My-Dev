18bc85dae01d68ea31c81bc3e43204b658dc198b
Alberto Pérez de Rada Fiol <apdrf.94@gmail.com>
2020-06-03 17:40:11 +0200

    Add a basic `fetch_api()` function

--- oh-my-dev.plugin.zsh
+++ oh-my-dev.plugin.zsh
@@ -1,5 +1,16 @@
 # dev.to search from terminal
 
-function oh_my_dev() {
+function fetch_api() {
+  base_url="https://dev.to/api";
+  path="/articles";
   
+  response=$(curl "$base_url$path");
+
+  return response;
 }
+
+function oh_my_dev() {
+  fetch_api;
+}
+  
+oh_my_dev;
