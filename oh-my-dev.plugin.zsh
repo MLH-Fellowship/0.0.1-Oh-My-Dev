0cfc2bf1a07c9660e9cbda89da5f0167b2387f9f
Alberto Pérez de Rada Fiol <apdrf.94@gmail.com>
2020-06-04 16:05:03 +0200

    Preview fetched articles on the console

--- oh-my-dev.plugin.zsh
+++ oh-my-dev.plugin.zsh
@@ -10,7 +10,46 @@ function fetch_api() {
 }
 
 function oh_my_dev() {
+  user_exit=false;
+  echo "Fetching the last 30 articles from https://dev.to";
+  echo;
   api_response=$(fetch_api);
+  echo;
+  echo "Done";
+
+  echo;
+  echo "Previewing articles:";
+  for row in $(jq -r '.[] | @base64' <<< $api_response); do
+    _jq() {
+      echo $row | base64 --decode | jq -r $1;
+    }
+
+    echo;
+    eval $(_jq '@sh "title=\(.title) description=\(.description) name=\(.user.username) date=\(.readable_publish_date) url=\(.canonical_url)"');
+    echo $title;
+    echo "  - Published by $name on $date";
+    echo;
+    echo $description;
+    echo;
+    echo "Read the full article on $url";
+    echo;
+
+    if read -q "choice?Press Y/y to preview another article, or any other key to cancel: "; then
+      echo;
+      continue;
+    else
+      user_exit=true;
+      echo;
+      echo;
+      echo "See you later!";
+      break;
+    fi
+  done
+
+  if [[ $user_exit = false ]]; then
+    echo;
+    echo "We have no more articles, please come back later!";
+  fi
 }
 
 oh_my_dev;
