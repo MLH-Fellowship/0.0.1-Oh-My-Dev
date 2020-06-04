b26a7509eb9b7ef142292aba4f1611adfbdafe7d
Alberto Pérez de Rada Fiol <apdrf.94@gmail.com>
2020-06-04 17:53:44 +0200

    Add colors and separator

--- oh-my-dev.plugin.zsh
+++ oh-my-dev.plugin.zsh
@@ -9,6 +9,10 @@ function fetch_api() {
   echo $response;
 }
 
+function color() {
+  echo $fg[$1]$2$reset_color;
+}
+
 function oh_my_dev() {
   user_exit=false;
   echo "Fetching the last 30 articles from https://dev.to";
@@ -25,13 +29,15 @@ function oh_my_dev() {
     }
 
     echo;
+    echo "----------";
     eval $(_jq '@sh "title=\(.title) description=\(.description) name=\(.user.username) date=\(.readable_publish_date) url=\(.canonical_url)"');
-    echo $title;
-    echo "  - Published by $name on $date";
+    echo $(color yellow $title);
+    echo "  Published by $(color yellow $name) on $(color yellow $date)";
     echo;
-    echo $description;
+    echo $(color yellow $description);
     echo;
-    echo "Read the full article on $url";
+    echo "Read the full article on $(color yellow $url)";
+    echo "----------";
     echo;
 
     if read -q "choice?Press Y/y to preview another article, or any other key to cancel: "; then
