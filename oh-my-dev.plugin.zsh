167d83e8c27ecc03f2863d72f7a11a3117d9dd79
Prosper Opara <oparaprosper79@gmail.com>
2020-06-04 18:04:21 +0100

    Refactor logic

--- oh-my-dev.plugin.zsh
+++ oh-my-dev.plugin.zsh
@@ -10,32 +10,42 @@ function fetch_api() {
 }
 
 function ohmydev_tag() {
-  counter=0
-  FGBLUE=`echo "\033[35m"`
-  NORMAL=`echo "\033[m"`
+  counter=0;
+  FGBLUE=`echo "\033[35m"`;
+  NORMAL=`echo "\033[m"`;
 
   base_url="https://dev.to/api/articles?tag=$1&per_page=9";
+  echo "${FGBLUE}fetching articles with tagged $1... ${NORMAL}";
   response=$(curl $base_url);
   
-  title=$(echo $response | jq ".[$counter].title")
-  description=$(echo $response | jq ".[$counter].title")
-  url=$(echo $response | jq ".[$counter].url")
+  title=$(echo $response | jq ".[$counter].title");
+  description=$(echo $response | jq ".[$counter].title");
+  url=$(echo $response | jq ".[$counter].url");
+
+  echo;
+  echo "------------------------------------------------------------";
+  echo "${FGBLUE}title: ${NORMAL}$title \n${FGBLUE}description: ${NORMAL}$description \n${FGBLUE}url: ${NORMAL}$url";
+  echo "------------------------------------------------------------";
+  echo;
   
-  echo "\n${FGBLUE}title: ${NORMAL}$title \n${FGBLUE}description: ${NORMAL}$description \n${FGBLUE}url: ${NORMAL}$url\n"
-  
-  read "?See more [y/Y] [n/N] " input
+  read "?See more [y/Y] [n/N] " input;
   while [ $input = "y" ]
   do
-    counter=$((counter+1))
+    counter=$((counter+1));
 
-    title=$(echo $response | jq ".[$counter].title")
-    description=$(echo $response | jq ".[$counter].title")
-    url=$(echo $response | jq ".[$counter].url")
+    title=$(echo $response | jq ".[$counter].title");
+    description=$(echo $response | jq ".[$counter].title");
+    url=$(echo $response | jq ".[$counter].url");
+
+    echo;
+    echo "------------------------------------------------------------";
+    echo "${FGBLUE}title: ${NORMAL}$title \n${FGBLUE}description: ${NORMAL}$description \n${FGBLUE}url: ${NORMAL}$url";
+    echo "------------------------------------------------------------";
+    echo;
   
-    echo "\n${FGBLUE}title: ${NORMAL}$title \n${FGBLUE}description: ${NORMAL}$description \n${FGBLUE}url: ${NORMAL}$url\n"
-  
-    read "?See more [y/Y] [n/N] " input
+    read "?See more [y/Y] [n/N] " input;
   done
+  echo "See you next time!"
 }
 
 function color() {
@@ -87,3 +97,4 @@ alias oh_my_dev='oh_my_dev';
 
 # test locally by calling ohmydev_tag on the next line
 alias oh_my_dev_tag="ohmydev_tag $1";
+ohmydev_tag $1;
