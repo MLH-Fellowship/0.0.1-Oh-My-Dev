eea777230d0d0a73e3b0b82814dd8395af16f844
Prosper Opara <oparaprosper79@gmail.com>
2020-06-05 11:53:17 +0100

    add WIP

--- oh-my-dev.plugin.zsh
+++ oh-my-dev.plugin.zsh
@@ -3,15 +3,33 @@
 function fetch_api() {
   base_url="https://dev.to/api";
   url_path="/articles";
-  url="$base_url$url_path$1?tag=$1";
+  url="$base_url$url_path?tag=$1&per_page=3";
   
   response=$(curl $url);
   echo $response;
 }
 
-function ohmydev_tag() {
+function color() {
+  echo $fg[$1]$2$reset_color;
+}
+
+function format_and_print_article() {
+  echo;
+  echo "----------";
+  eval $(echo $1 | base64 --decode | jq -r '@sh "title=\(.title) description=\(.description) name=\(.user.username) date=\(.readable_publish_date) url=\(.canonical_url)"');
+  echo $(color yellow $title);
+  echo "  Published by $(color yellow $name) on $(color yellow $date)";
+  echo;
+  echo $(color yellow $description);
+  echo;
+  echo "Read the full article on $(color yellow $url)";
+  echo "----------";
+  echo;
+}
+
+function oh_my_dev() {
   user_exit=false;
-  echo "Fetching the articles tagged $1";
+  echo "Fetching the last 30 articles from https://dev.to";
   echo;
   api_response=$(fetch_api $1);
   echo;
@@ -40,35 +58,18 @@ function ohmydev_tag() {
   fi
 }
 
-function color() {
-  echo $fg[$1]$2$reset_color;
-}
-
-function format_and_print_article() {
-  echo;
-  echo "----------";
-  eval $(echo $1 | base64 --decode | jq -r '@sh "title=\(.title) description=\(.description) name=\(.user.username) date=\(.readable_publish_date) url=\(.canonical_url)"');
-  echo $(color yellow $title);
-  echo "  Published by $(color yellow $name) on $(color yellow $date)";
-  echo;
-  echo $(color yellow $description);
-  echo;
-  echo "Read the full article on $(color yellow $url)";
-  echo "----------";
-  echo;
-}
-
-function oh_my_dev() {
+function ohmydev_tag() {
   user_exit=false;
-  echo "Fetching the last 30 articles from https://dev.to";
+  echo "Fetching the articles tagged $1";
   echo;
-  api_response=$(fetch_api);
+  api_response=$(fetch_api $1);
   echo;
   echo "Done";
 
   echo;
   echo "Previewing articles:";
   for row in $(jq -r '.[] | @base64' <<< $api_response); do
+  echo $row
     format_and_print_article $row;
 
     if read -q "choice?Press Y/y to preview another article, or any other key to cancel: "; then
@@ -94,3 +95,4 @@ alias oh_my_dev='oh_my_dev';
 # test locally by calling ohmydev_tag on the next line
 alias oh_my_dev_tag="ohmydev_tag $1";
 ohmydev_tag $1;
+# oh_my_dev $1;
